21d88cf2e712de2cf27dffa872e0fc03d8a4ff47
sella muthu <sella muthu@DESKTOP-DLO54QO>
2021-04-11 19:34:55 +0530

    correction

--- Basics/src/Stringhomework/Totalnoofcharacters.java
+++ Basics/src/Stringhomework/Totalnoofcharacters.java
@@ -5,8 +5,19 @@ public class Totalnoofcharacters {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		String s="malala got nobel prize! for peace in swizz";
+		int count=0;
+		char c[]=new char[s.length()];
+		for(int i=0; i<c.length; i++)
+		{
+		c[i]=s.charAt(i);
+		if(' '==c[i])
+		{
+			 count=count+1;
+		}
+		
+		}//System.out.println(count);
 	
-		System.out.println(s.length());
+		System.out.println(s.length()-count);
 		
 
 	}
