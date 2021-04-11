c71911ef06834e83b0bec360743f218fa13d7b60
sella muthu <sella muthu@DESKTOP-DLO54QO>
2021-04-11 12:44:47 +0530

    qwerty

--- Basics/src/Array/FindANumber.java
+++ Basics/src/Array/FindANumber.java
@@ -4,7 +4,7 @@ public class FindANumber {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int a[]={5,2,4,5,6,8};
+		int a[]={5,2,4,5,6,0,8};
 		int search=5;
 		boolean flag =false;
 		for(int i=0;i<a.length; i++)
