1248e348540520e7edde265628209cd5fd27bbea
sella muthu <sella muthu@DESKTOP-DLO54QO>
2021-04-11 11:55:46 +0530

    comparing strings

---
+++ Basics/src/String/Stringcompare.java
@@ -0,0 +1,15 @@
+package String;
+
+public class Stringcompare {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String a="azar";
+		String b="zara";
+		System.out.println(a.compareTo(b));
+		
+		System.out.println(b.compareTo(a));
+
+	}
+
+}
