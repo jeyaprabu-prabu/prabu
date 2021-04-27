6c61d0f116169db40e5ae8ae4a6a4bde849b291e
sella muthu <sella muthu@DESKTOP-DLO54QO>
2021-04-27 08:29:47 +0530

    hi

--- Basics/src/JCF/Enter_n_numbers_until_q_is_entered.java
+++ Basics/src/JCF/Enter_n_numbers_until_q_is_entered.java
@@ -7,7 +7,7 @@ public class Enter_n_numbers_until_q_is_entered {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-	ArrayList al= new ArrayList();
+	ArrayList<String> al= new ArrayList();
 	Scanner sc= new Scanner(System.in);
 	String s=" ";
 	while(! s.equalsIgnoreCase("q"));
--- Basics/src/JCF/LinkedList_full_of_numbers_with_duplicates_tx_into_LinkedHashset.java
+++ Basics/src/JCF/LinkedList_full_of_numbers_with_duplicates_tx_into_LinkedHashset.java
@@ -7,7 +7,7 @@ public class LinkedList_full_of_numbers_with_duplicates_tx_into_LinkedHashset {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-          LinkedList ll=new LinkedList();
+          LinkedList<Integer> ll=new LinkedList();
           ll.add(2);
           ll.add(4);
           ll.add(6);
